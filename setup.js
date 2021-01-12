1d2b57cd92abbf73dc1b98ae18e4f8a069e351cd
Cole Schultz <schultzcole@gmail.com>
2021-01-12 10:33:56 -0600

    Attempt to add damage context menu items for damage cards

--- setup.js
+++ setup.js
@@ -1,7 +1,9 @@
-import { patchItemRollDamage } from "./scripts/itemPatch.js";
 import { MODULE_TITLE, MODULE_TITLE_SHORT } from "./scripts/const.js";
+import { patchItemRollDamage } from "./scripts/itemPatch.js";
+import { patchChatLogContextMenu } from "./scripts/chatLogPatch.js";
 
 Hooks.on("setup", () => {
     console.log(`${MODULE_TITLE_SHORT} | Initializing ${MODULE_TITLE}`);
     patchItemRollDamage();
+    patchChatLogContextMenu();
 })
