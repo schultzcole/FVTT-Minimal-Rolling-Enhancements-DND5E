85dc04c6bfeab46524a0b57537613f2883f99505
Cole Schultz <schultzcole@gmail.com>
2021-01-12 17:17:59 -0600

    Add a unified damage dialog that applies to all roll parts for an item damage roll

--- scripts/itemDamagePatch.js
+++ scripts/itemDamagePatch.js
@@ -4,22 +4,48 @@ import { getSettingLocalOrDefault } from "./settings.js";
 
 export function patchItemRollDamage() {
     libWrapper.register(MODULE_NAME, "CONFIG.Item.entityClass.prototype.rollDamage", async function (wrapped, ...args) {
-        let {critical=false, /** @type MouseEvent */ event=null, options={}} = args[0];
+        let {/** @type MouseEvent */ event=null, options={}} = args[0];
 
-        let noFastForwardModifier = getSettingLocalOrDefault("showRollDialogModifier");
+        let showDamageDialog = getSettingLocalOrDefault("showRollDialogModifier");
 
-        const optionsOverride = {
-            fastForward: !event[noFastForwardModifier],
-        };
+        const title = `${this.name} - ${game.i18n.localize("DND5E.DamageRoll")}`;
+        let rollMode = options.rollMode ?? game.settings.get("core", "rollMode");
+        let critical = false;
+        let bonus = null;
 
-        args[0].options = mergeObject(optionsOverride, critical, options);
+        // Show a custom dialog that applies to all damage parts
+        if (event[showDamageDialog]) {
+            const dialogOptions = {
+                top: event ? event.clientY - 80 : null,
+                left: window.innerWidth - 710,
+            };
+            const dialogData = await _damageDialog({ title, rollMode, dialogOptions });
+
+            if (!dialogData) return null;
+
+            critical = dialogData?.critical ?? critical;
+            rollMode = dialogData?.rollMode ?? rollMode;
+            bonus = dialogData?.bonus ?? bonus;
+        }
+
+        // Skip core dnd5e dialog
+        if (!args[0].options) args[0].options = {};
+        args[0].options.fastForward = true;
+        args[0].critical = critical;
 
         if (!this.data.data.damage?.parts) throw new Error("You cannot roll damage for this item.");
 
         const partRolls = await _rollDamageParts(this, wrapped, args[0]);
+        if (bonus) {
+            const bonusRoll = new Roll(bonus);
+            if (critical) {
+                bonusRoll.alter(2, 0);
+            }
+            partRolls.push({ roll: bonusRoll.roll(), flavor: game.i18n.localize("DND5E.RollSituationalBonus").slice(0, -1) })
+        }
         const renderedRolls = await _renderCombinedDamageRollCard(partRolls);
 
-        const messageData = await _createCombinedDamageMessageData(this, renderedRolls, critical, options);
+        const messageData = await _createCombinedDamageMessageData(this, title, renderedRolls, critical, rollMode, options);
 
         // Show DSN 3d dice if available
         if (game.dice3d) {
@@ -33,6 +59,48 @@ export function patchItemRollDamage() {
         // Return the array of Roll objects
         return partRolls;
     }, "MIXED");
+}
+
+/**
+ * @returns {Promise<{ critical: Boolean, bonus: Number, rollMode: String }>}
+ * @private
+ */
+async function _damageDialog({ title, rollMode, dialogOptions }={}) {
+    const template = "systems/dnd5e/templates/chat/roll-dialog.html";
+    const dialogData = {
+        formula: "",
+        rollMode: rollMode,
+        rollModes: CONFIG.Dice.rollModes,
+    };
+    const content = $(await renderTemplate(template, dialogData));
+
+    content.find("[name=formula]").closest(".form-group").remove();
+
+    return new Promise(resolve => {
+        new Dialog({
+            title,
+            content: content.prop("outerHTML"),
+            buttons: {
+                critical: {
+                    label: game.i18n.localize("DND5E.CriticalHit"),
+                    callback: html => resolve({ critical: true, ..._parseDamageDialog(html[0].querySelector("form")) })
+                },
+                normal: {
+                    label: game.i18n.localize("DND5E.Normal"),
+                    callback: html => resolve({ critical: false, ..._parseDamageDialog(html[0].querySelector("form")) })
+                },
+            },
+            default: "normal",
+            close: () => resolve(null)
+        }, dialogOptions).render(true);
+    });
+}
+
+function _parseDamageDialog(form) {
+    return form ? {
+        bonus: form.bonus.value,
+        rollMode: form.rollMode.value,
+    } : {};
 }
 
 async function _rollDamageParts(item, innerRollDamage, { critical, event, spellLevel, versatile, options }) {
@@ -74,11 +142,12 @@ async function _renderCombinedDamageRollCard(rolls) {
     // Assemble them under one container div
     const content = $("<div class=\"mre-damage-card\">");
     content.append(renderedRolls);
+    content.find(".dice-roll:not(:last-child)").after("<hr />");
 
     return content.prop("outerHTML");
 }
 
-async function _createCombinedDamageMessageData(item, content, critical, options) {
+async function _createCombinedDamageMessageData(item, flavor, content, critical, rollMode, options) {
     // This decoy roll is used to convince foundry that the message has ROLL type
     const decoyRoll = new Roll("0").roll();
 
@@ -86,7 +155,7 @@ async function _createCombinedDamageMessageData(item, content, critical, options
     const messageData = {
         user: game.user._id,
         content,
-        flavor: `${item.name} - ${game.i18n.localize("DND5E.DamageRoll")}`,
+        flavor,
         roll: decoyRoll,
         speaker: ChatMessage.getSpeaker({actor: item.actor}),
         type: CONST.CHAT_MESSAGE_TYPES.ROLL,
@@ -100,7 +169,6 @@ async function _createCombinedDamageMessageData(item, content, critical, options
         messageData["flags.dnd5e.roll"].critical = true;
     }
 
-    const rollMode = options.rollMode ?? game.settings.get("core", "rollMode");
     ChatMessage.applyRollMode(messageData, rollMode);
 
     // Merge with data passed into the function call
