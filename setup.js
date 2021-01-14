91f5e673d46676a73d1310072df27590d4441223
Cole Schultz <schultzcole@gmail.com>
2021-01-14 02:53:07 -0600

    Rename files to match snake-case convention

--- setup.js
+++ setup.js
@@ -1,9 +1,9 @@
 import { MODULE_TITLE, MODULE_TITLE_SHORT } from "./scripts/const.js";
-import { patchChatLogContextMenu } from "./scripts/chatLogPatch.js";
-import { patchAbilityChecks } from "./scripts/abilityCheckPatches.js";
-import { patchItemBaseRoll } from "./scripts/itemBaseRollPatch.js";
-import { patchItemRollDamage } from "./scripts/itemDamagePatch.js";
-import { patchItemPrepareData } from "./scripts/itemPrepareDataPatch.js";
+import { patchChatLogContextMenu } from "./scripts/chat-log-patch.js";
+import { patchAbilityChecks } from "./scripts/ability-check-patches.js";
+import { patchItemBaseRoll } from "./scripts/item-base-roll-patch.js";
+import { patchItemRollDamage } from "./scripts/item-damage-patch.js";
+import { patchItemPrepareData } from "./scripts/item-prepare-data-patch.js";
 import { registerSettings } from "./scripts/settings.js";
 
 Hooks.on("setup", () => {
