7ca51a80c5676cefb032a540566bf19a6198e659
Cole Schultz <schultzcole@gmail.com>
2021-01-20 19:27:01 -0600

    When rolling separate damage rolls, attach a combined roll to the associated message
    - Previously we used a "decoy" empty roll for split damage rolls. This change instead combines all of the individual rolls into a single roll that is attached to the chat message
    - This has the benefit that DSN will automatically roll dice for all of the individual rolls in the card, without us manually needing to call showForRoll
    - It also has the benefit that if the user right clicks on an area of the chat card that is not an individual roll, they can correctly apply the total results of the combined rolls

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,17 @@
 export const pause = (millis) => new Promise(resolve => setTimeout(resolve, millis));
+
+/**
+ * Combines multiple Roll objects into a single merged Roll.
+ * @param {Roll} rolls
+ */
+export function combineRolls(...rolls) {
+    const roll = new Roll("0");
+    roll.data = duplicate(rolls[0].data);
+    roll.results = rolls.reduce((prev, next) => prev.length ? [...prev, "+", ...next.results] : next.results, []);
+    roll.terms = rolls.reduce((prev, next) => prev.length ? [...prev, "+", ...next.terms] : next.terms, []);
+    roll._dice = rolls.reduce((prev, next) => prev.length ? [...prev, "+", ...next._dice] : next._dice, []);
+    roll._formula = rolls.map(r => r._formula).join(" + ");
+    roll._rolled = true;
+    roll._total = rolls.reduce((acc, next) => acc + next._total, 0);
+    return roll;
+}
