de37f00b380fc7f8f6b1d6f9c97988f3195d6e00
Cole Schultz <schultzcole@gmail.com>
2022-05-17 06:48:41 -0500

    disable inputs when sheet is not editable
    - this can happen if the item is in a locked compendium or if the
    current user is not an owner (I think)

--- scripts/hooks/render-item-5e-sheet.mjs
+++ scripts/hooks/render-item-5e-sheet.mjs
@@ -2,12 +2,14 @@ import { MODULE_NAME } from "../const.mjs";
 import { FormulaGroupConfig } from "../apps/formula-group-config.mjs";
 
 Hooks.on("renderItemSheet5e", (itemSheet, html, _) => {
+    const locked = !itemSheet.isEditable;
+
     // Add formula group config button
     const tooltip = game.i18n.localize(`${MODULE_NAME}.FORMULA-GROUP.DialogTitle`);
     const damageHeader = html.find(".tab.details .damage-header");
     damageHeader.wrap(`<div class="mre-damage-header-container">`);
     damageHeader.before(`<a title="${tooltip}" class="config-formula-groups"><i class="fas fa-tasks"></i></a>`);
-    damageHeader.after(_makeAutoRollCheckboxElement(itemSheet.document, "Damage", true));
+    damageHeader.after(_makeAutoRollCheckboxElement(itemSheet.document, "Damage", true, locked));
 
     // Open the formula group config when the user clicks on the button
     html.find(".config-formula-groups").click(() => new FormulaGroupConfig(itemSheet.document, { editable: itemSheet.isEditable }).render(true) );
@@ -20,24 +22,27 @@ Hooks.on("renderItemSheet5e", (itemSheet, html, _) => {
         // Only show the auto-roll checkbox button if actionType is populated
         const actionType = html.find(`.tab.details`).find(`[name="data.actionType"]`);
         actionType.wrap(`<div class="form-fields">`);
-        actionType.after(_makeAutoRollCheckboxElement(itemSheet.document, "Attack", true));
+        actionType.after(_makeAutoRollCheckboxElement(itemSheet.document, "Attack", true, locked));
     }
 
     const otherFormula = html.find(`.tab.details`).find(`[name="data.formula"]`);
-    otherFormula.after(_makeAutoRollCheckboxElement(itemSheet.document, "Other", false));
+    otherFormula.after(_makeAutoRollCheckboxElement(itemSheet.document, "Other", false, locked));
 
-    // Handle "checkbox" button clicks
-    html.on('click', `.tab.details input[type=checkbox]:not(.three-way)`, (event) => _handleTwoWayCheckboxButtonPress(event, itemSheet.document));
-    html.on('click', `.tab.details input[type=checkbox].three-way`, (event) => _handleThreeWayCheckboxButtonPress(event, itemSheet.document));
+    if (!locked) {
+        // Handle "checkbox" button clicks
+        html.on('click', `.tab.details input[type=checkbox]:not(.three-way)`, (event) => _handleTwoWayCheckboxButtonPress(event, itemSheet.document));
+        html.on('click', `.tab.details input[type=checkbox].three-way`, (event) => _handleThreeWayCheckboxButtonPress(event, itemSheet.document));
+    }
 });
 
 // only present if the Items With Rollable Tables module is present
 Hooks.on('items-with-rolltables-5e.sheetMutated', (itemSheet, html) => {
+    const locked = !itemSheet.isEditable;
     const rollableTable = html.find(`.tab.details`).find('.rollable-table-drop-target');
-    rollableTable.append(_makeAutoRollCheckboxElement(itemSheet.document, "Rolltable", true));
+    rollableTable.append(_makeAutoRollCheckboxElement(itemSheet.document, "Rolltable", true, locked));
 })
 
-function _makeAutoRollCheckboxElement(item, target, threeWay) {
+function _makeAutoRollCheckboxElement(item, target, threeWay, locked) {
     const text = game.i18n.localize(`${MODULE_NAME}.AUTO-ROLL.AutoRoll`);
     const element = $(`<label class="mre-auto-roll"><input type="checkbox" /><span class="label">${text}</span></label>`);
 
@@ -48,6 +53,7 @@ function _makeAutoRollCheckboxElement(item, target, threeWay) {
     const checkbox = element.find("input[type=checkbox]")[0];
     checkbox.checked = flag;
     checkbox.dataset.autoRollTarget = target;
+    checkbox.disabled = locked
     if (threeWay) {
         checkbox.indeterminate = flag === undefined;
         checkbox.dataset.state = _threeWayState(flag);
